6a8629be7ea5aae0f7778e15fc8dd0c3875c4227
刘佳朋 <2395914432@qq.com>
2024-06-16 15:41:26 +0800

    列表渲染

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { onMounted } from 'vue'
+import { ref } from 'vue'
 import Edit from './components/Edit.vue'
+import axios from 'axios'
+// TODO: 列表渲染 ，声明响应式list，调用接口获取数据，后端数据赋值给list，绑定到table组件
+const list = ref([])
+const getList = async () =>{ // 调用接口的函数，用的是axios，同步的写法，在created或mounted里调用
+  const res = await axios.get('./list')
+  list.value = res.data
+}
 
-// TODO: 列表渲染
-
+onMounted(() => getList())
 
 // TODO: 删除功能
 
@@ -13,11 +21,7 @@ import Edit from './components/Edit.vue'
 
 <template>
   <div class="app">
-    <el-table :data="[{
-      id: 1,
-      name: 'jack',
-      place: 'none'
-    }]">
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
