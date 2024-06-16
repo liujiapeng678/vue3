e48c90de8b83f66cb48c6bada64a735e16cbdb01
刘佳朋 <2395914432@qq.com>
2024-06-16 11:56:37 +0800

    watch，生命周期函数，父子组件通信

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# 黑马程序员小兔鲜儿Vue3前端项目
+
 # vue3
 
 This template should help get you started developing with Vue 3 in Vite.
--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,28 @@
-<!-- <script>
-export default {
-  setup(){
-    const message = 'this is message'
-    const logMessage = ()=>{
-      console.log(message)
-    }
-    return {
-      message,
-      logMessage
-    }
-  }
-}
-</script> -->
-<!-- <script setup>
-  import { reactive } from 'vue'//导入函数
-  //执行函数，传入一个对象类型的参数，响应式对象可以引发视图的更新
-  //ref函数既支持简单类型也支持对象，返回响应式的对象
-  const state = reactive({
-    count: 0
-  })
-  const setCount = () => {
-    state.count++
-  }
-</script> -->
-<!-- <script setup>
-  import { ref } from 'vue'
-  const count = ref(0)
-  //脚本区域修改ref产生的相应式数据，通过.value属性
-  const setCount = () => {
-    count.value++
-  }
-</script> -->
-<!-- computed计算属性函数 return 基于相应式数据做计算的值-->
+<!-- 
+//执行函数，传入一个对象类型的参数，响应式对象可以引发视图的更新
+//ref函数既支持简单类型也支持对象，返回响应式的对象
+//脚本区域修改ref产生的相应式数据，通过.value属性
+computed计算属性函数 return 基于相应数据做计算的值 -->
+<!-- watch函数用来监听单一或多个数据变化，数据变化时执行回调函数，两个额外参数对象1.immediate（第一次就立即执行，变化时也执行），2.deep（深度侦听）
+例如state = ref({count: 0}) state.value.count++,state会改变，但不会触发侦听state的watch的回调函数，这时需要deep，如果前面用reactive，reactive默认开启deep。
+如果对象中除了count还有age，但又不想监听age，在watch中再写一个回调函数，不用deep，指出想要监听谁，deep有性能损耗，一般不开启 -->
+<!-- 生命周期函数 创建挂载更新卸载 选项式API->组合式API 引入函数，执行函数传入回调， -->
+
+组合式API父子通信，
 
 <script setup>
   import { ref } from 'vue'
-  import { computed } from 'vue'
-  const list = ref([1, 2, 3, 4, 5, 6, 7, 8])
-  const computedList = computed(()=>{
-    return list.value.filter(item => item > 2)
+  import { watch } from 'vue'
+  const count = ref(0)
+  const setCount = () => {
+    count.value++
+  }
+  watch(count, (newValue, oldValue) => {
+    console.log(newValue, oldValue)
   })
 </script>
 <template>
   <div>
-    原始响应式数据 {{ list }}
-  </div>
-  <div>
-    计算属性数组 {{ computedList }}
+    <button @click="setCount">{{ count }}</button>
   </div>
 </template>
