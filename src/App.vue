50ce1c60bb2f944e8ef373264acacf8510201c88
刘佳朋 <2395914432@qq.com>
2024-06-16 17:00:26 +0800

    编辑

--- src/App.vue
+++ src/App.vue
@@ -14,29 +14,34 @@ onMounted(() => getList())
 
 // TODO: 删除功能，获取当前行的id，通过id调用删除接口，更新最新的列表
 const onDelete = async (id) => {
-  await axios.delete('/del/${id}')
+  await axios.delete(`/del/${id}`)
   getList()
 }
 
-// TODO: 编辑功能
+// TODO: 编辑功能，打开弹框（获取子组件edit.vue实例，调用方法或修改属性），回填数据（调用详情接口/当前行的静态数据），更新数据
+const editRef = ref(null)
+const onEdit = (row) => {
+  editRef.value.open(row)
+}
+
 
 </script>
 
 <template>
   <div class="app">
-    <el-table :data="list">
+    <el-table :data="list">3
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
         <template #default="{ row }">
-          <el-button type="primary" link>编辑</el-button>
+          <el-button type="primary" @click="onEdit(row)" link>编辑</el-button>
           <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref="editRef" @on-update="getList"/>
 </template>
 
 <style scoped>
